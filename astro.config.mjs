11fd5a9f29b8973b3f42776a006404307ce75df8
Dennis Macapagal <macapagal.dennis@gmail.com>
2024-08-17 01:12:55 +0800

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-    site: 'https://FHGdev.github.io/',
+    site: 'https://fhgdev.github.io/',
     base: '/sg/',
     integrations: [sitemap()],
 });
